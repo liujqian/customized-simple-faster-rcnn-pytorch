a9e94c20ce132746fa710e678a6935ffcd16d15d
Jingqian Liu <liujqian@hotmail.com>
2023-03-17 14:26:31 -0700

    added the rpn-only inferences.

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import numpy as np
 import torch as t
@@ -11,41 +12,50 @@ from utils.vis_tool import vis_bbox
 from utils import array_tool as at
 
 if __name__ == '__main__':
-    img = read_image('datasets/webis-webseg-20/webis-webseg-20-screenshots/009514.png')
-    img = t.from_numpy(img)[None]
     faster_rcnn = FasterRCNNVGG16()
     faster_rcnn.rpn.proposal_layer.nms_thresh = 0.3
     trainer = FasterRCNNTrainer(faster_rcnn).cuda()
     trainer.load('checkpoints-rpn-only/fasterrcnn_epoch_best_best_map_0.13068130595527067')
-    opt.caffe_pretrain = False  # this model was trained from caffe-pretrained model
-    original_roi,roi_scores = trainer.faster_rcnn.predict(img, visualize=True)
-    bbox_polygon_list = []
-    for bbox in original_roi[:20]:  # (y_{min}, x_{min}, y_{max}, x_{max})
-        bbox_int = bbox.astype(np.int32)
-        left = bbox_int[1]
-        top = bbox_int[0]
-        right = bbox_int[3]
-        bottom = bbox_int[2]
-        bbox_polygon_list.append(
-            [
+    ids = [d.name.split(".")[0] for d in os.scandir("datasets/webis-webseg-20/webis-webseg-20-screenshots") if
+           int(d.name.split(".")[0]) > 9487]
+    inference_times = []
+    for i, img_id in enumerate(ids):
+        img = read_image(f'datasets/webis-webseg-20/webis-webseg-20-screenshots/{img_id}.png')
+        img = t.from_numpy(img)[None]
+        opt.caffe_pretrain = False  # this model was trained from caffe-pretrained model
+        inference_start_time = time.time()
+        original_roi, roi_scores = trainer.faster_rcnn.predict(img, visualize=True)
+        inference_times.append(time.time() - inference_start_time)
+        print(f"Just finished the {i}th inference out of {len(ids)}. This one took {inference_times[-1]}seconds!")
+        bbox_polygon_list = []
+        for bbox in original_roi[:100]:  # (y_{min}, x_{min}, y_{max}, x_{max})
+            bbox_int = bbox.astype(np.int32)
+            left = bbox_int[1]
+            top = bbox_int[0]
+            right = bbox_int[3]
+            bottom = bbox_int[2]
+            bbox_polygon_list.append(
                 [
                     [
-                        [left.item(), top.item()],
-                        [left.item(), bottom.item()],
-                        [right.item(), bottom.item()],
-                        [right.item(), top.item()],
-                        [left.item(), top.item()],
+                        [
+                            [left.item(), top.item()],
+                            [left.item(), bottom.item()],
+                            [right.item(), bottom.item()],
+                            [right.item(), top.item()],
+                            [left.item(), top.item()],
+                        ],
                     ],
                 ],
-            ],
+            )
+        out_obj = dict(
+            height=img.shape[2],
+            width=img.shape[3],
+            id="009514",
+            segmentations=dict(
+                faster_rcnn_rpn_only_bboxes=bbox_polygon_list,
+            ),
         )
-    out_obj = dict(
-        height=img.shape[2],
-        width=img.shape[3],
-        id="009514",
-        segmentations=dict(
-            mmdetection_bboxes=bbox_polygon_list,
-        ),
-    )
-    with open("temp/009514_rois.json", "w") as handle:
-        json.dump(out_obj, handle)
+        with open(f"inferences/rpn-only/screenshots/{img_id}.json", "w") as handle:
+            json.dump(out_obj, handle)
+        # The average inference time is 0.07559035852622081
+        print(f"The average inference time is {sum(inference_times) / len(inference_times)}")
